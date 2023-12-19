12e1eef2e31771e5c039a69c4cc5fad6778ed50d
AndreaParrini <andrea.parrini26@gmail.com>
2023-12-19 21:45:10 +0100

    Initial Commit
    Create file index.html and script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const news = [
+    {
+        id: 1,
+        title: 'Scoperta di una nuova specie di papera di gomma',
+        content: 'Scoperta di una nuova specie di papera di gomma.',
+        tags: ['geo', 'tech'],
+        author: 'Diana Rossi',
+        published: Date('2023-02-11'),
+        img: './images/rubber-duck.jpg'
+    },
+    {
+        id: 2,
+        title: 'Esplorando le profondità marine: il mistero degli abissi',
+        content: 'Esplorando le profondità marine: il mistero degli abissi',
+        tags: ['geo', 'viaggi'],
+        author: 'Fabio Mari',
+        published: Date('2023-03-14'),
+        img: './images/kitchen-food.jpg'
+    },
+    {
+        id: 3,
+        title: 'Viaggio culinario: alla ricerca dei sapori perduti',
+        content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici.',
+        tags: ['cucina'],
+        author: 'Marta Bianchi',
+        published: Date('2023-04-20'),
+        img: './images/deep-sea.jpg'
+    },
+    {
+        id: 4,
+        title: 'Arte moderna: oltre i confini convenzionali',
+        content: "Un'analisi delle tendenze e delle sfide nell'arte contemporanea, con interviste a artisti emergenti.",
+        tags: ['arte', 'tech'],
+        author: 'Gabriele Neri',
+        published: Date('2023-05-2'),
+        img: './images/modern-art.jpg'
+    }
+]
+
